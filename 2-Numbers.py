9fe5f6a194a1c4d5492dfc41f879e206743fc40e
Parsa Soroush <parsakingkp@gmail.com>
2025-05-27 18:41:25 +0330

    Numbers: All numbers operations

---
+++ 2-Numbers.py
@@ -0,0 +1,20 @@
+# Plus
+print(2 + 2)
+
+# Minus
+print(2 - 3)
+
+# Multiplication
+print(2 * 2)
+
+# Division
+print(3 / 2)
+
+# Floor Division
+print(4 // 2)
+
+# Modulo
+print(4 % 2)
+
+# Power
+print(3 ** 3)
